9430f11aae92d8fd1aa308b00108ea282c7329ad
gerto <gertobin11@gmail.com>
2025-04-28 07:57:24 +0100

    added a running app to implement tests

--- src/main/java/com/mycompany/thecinemaapp/TheCinemaApp.java
+++ src/main/java/com/mycompany/thecinemaapp/TheCinemaApp.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.thecinemaapp;
 
+import java.util.Scanner;
+
 /**
  *
  * @author gerto
@@ -11,6 +13,43 @@ package com.mycompany.thecinemaapp;
 public class TheCinemaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String email = "test@test.com";
+        String password = "test password";
+        Boolean authenticated = false;
+        Boolean rememberMe = false;
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        AuthController authController = new AuthController();
+        authController.registerUser(email, password, UserRole.USER);
+        
+        System.out.println("Welcome to The Cinema App\n");
+        System.out.println("Welcome to the login screen");
+        System.out.println("Please use these login details to login successfully yo the app\n");
+        System.out.println("Email: " + email);
+        System.out.println("Password: " + password);
+        System.out.println("\n ############\n");
+        
+        while (!authenticated) {
+            System.out.println("Please enter your email");
+            String enteredEmail = keyboard.next();
+            
+            System.out.println("Please enter your password: ");
+            String enteredPassword = keyboard.next();
+            
+            System.out.println("Do you want a long term login? Enter y else n: ");
+            String rememberMeInput = keyboard.next();
+            if (rememberMeInput.equalsIgnoreCase("y")) {
+                rememberMe = true;
+            }
+            
+            
+            if (authController.validateLogin(enteredEmail, enteredPassword)) {
+                authController.login(enteredEmail, enteredPassword, rememberMe);
+            }
+            
+            
+        }
+        
     }
 }
